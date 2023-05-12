e8ef8e84cf2caf6dcc99a75554db31408e942c14
lonkidely <inkandies.social@gmail.com>
2023-05-12 12:12:39 +0300

    prepare to work

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import {MainPage} from "./views/MainPage/mainPage";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={ <MainPage /> }/>
+    )
+);
+
+root.render(
+    <RouterProvider router={router}/>
+);

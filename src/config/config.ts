3d72dd26462b478297ed96483d779f0303c6971b
lonkidely <inkandies.social@gmail.com>
2023-05-22 01:25:12 +0300

    update url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-export const URL = 'https://192.168.178.102:49159';
+export const URL = 'https://localhost';
 
 export const FETCH_NOTES_URL = URL + '/GetNotes';
 export const ADD_NOTE_URL = URL + '/CreateNotes';
